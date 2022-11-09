787c4583c6d164c92f55bf8a0e0f67204abd55c0
Marcos <you@example.com>
2022-11-09 16:11:49 +0000

    Playing around with React Native

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Statusbar on top */}
+      <StatusBar style="auto"/>
+      
+      {/* Simple title */}
+      <Text style={styles.textTitle}>Welcome to a betterDay</Text>
+
+      {/* Button clickable */}
+      <Button title={"Here here 1"} />            
+      <Button title={"Here here 2"} />            
+      <Button title={"Here here 3"} />            
     </View>
   );
 }
 
+// Button interface
+interface ButtonProps {
+  title: string;
+}
+
+// Button example with a propertie
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}    
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+// Simple styles to start my project
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textTitle: {
+    color: '#000',
+    fontSize: 22,
   },
 });
